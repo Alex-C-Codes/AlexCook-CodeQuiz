b247b26b6c343ad983eb4cf8932923f7a3977b13
Alex-C <alexcook1613@gmail.com>
2022-10-23 21:36:13 -0400

    finished coding quiz. Added css style, reviewed finalized code, and added get high scores button

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,13 +52,11 @@ var myQuestions = [
     }
 ]
 
-// [x] When timerCount =< 0, game ends
-// [x] When last question, game ends
-// [ ] make game pretty
-// [ ] WHEN the game is over THEN I can save my initials and my score
+// Start Screen variables
+var startContainer = document.getElementById('startScreen');
 
 // Button variables
-var startButton = document.querySelector(".start-button");
+//var startButton = document.querySelector(".start-button");
 
 // Timer variables
 var timerContainer = document.getElementById("timer");
@@ -79,10 +77,22 @@ var answerContainers;
 var resultsContainer = document.getElementById('results');
 var numCorrect = 0;
 var resultsOutput = '';
+// highScores is an array that will store high scores via the numCorrect and initials var
+var userInitials;
+var highScore;
+var highScoresArr = [];
+var highScoresDisplayInitials = '';
+var highScoresDisplayScore = '';
+var highScoresDisplay = '';
 
 // Game Over Variables
 var gameOverContainer = document.getElementById('gameOverScreen');
 var initials;
+
+// Start Screen
+function getStartScreen() {
+    startContainer.innerHTML = '<div style="text-align:center;"><p>Try to answer the code quiz questions within the time limit. Please note that incorrect answers will reduce your time/score by 10 seconds!</p><br><input type="button" onclick="startQuiz()" value="Start Quiz" enabled/></div>'
+}
 
 // Timer function
 function startTimer() {
@@ -91,6 +101,7 @@ function startTimer() {
         timerContainer.innerHTML = '<div>Time Remaining: ' + timerCount + '</div>';
         if (timerCount === 0) {
             clearInterval(timer);
+            gameOver();
         }
     }, 1000);
 }
@@ -123,85 +134,98 @@ function givePrompts() {
             + '<input type="button" class="submit-button" onclick="submitAnswer()" value="Submit Answer">'
         );
 
-        //console.log(output);
-
         quizContainer.innerHTML = output.join('');
-        resultsContainer.innerHTML = '<br>Your Score: ' + numCorrect + '<br>' + resultsOutput;
+        //resultsContainer.innerHTML = '<br>Your Score: ' + numCorrect + '<br>' + resultsOutput;
+        resultsContainer.innerHTML = '<br>' + resultsOutput;
     } else {
+        console.log(timerCount);
         gameOver();
     }
 }
 
-// [ ] WHEN the game is over THEN I can save my initials and my score
-
 function gameOver() {
     // reset game conditions:
+    startContainer.innerHTML = '';
     quizContainer.innerHTML = ' ';
-    clearInterval(timer);
-    timerCount = 0;
-    timerContainer.innerHTML = '<div>Time Remaining: ' + timerCount + '</div>';
-    quizContainer.innerHTML = ' ';
-    startButton.disabled = false;
-    startButton.value = 'Start Quiz';
-    questionIndex = -1;
-
+    timerContainer.innerHTML = ' ';
+    console.log('timerCount is:');
+    console.log(timerCount);
+    let timerCountDisplay
+    if (timerCount > 0 ) {
+        timerCountDisplay = timerCount - 10;
+    } else {
+        timerCountDisplay = 0;
+    }
+    resultsContainer.innerHTML = '<br>Your Final Score: ' + timerCountDisplay + '<br>';
     gameOverContainer.innerHTML = '<br><label>Submit Initals: </label><input type="search" id="initialsInput"><input type="submit" onclick="handleClick()">'
+    clearInterval(timer);
+    questionIndex = -1
 }
 
-// function submitInitials() {
-//     initials = document.getElementById('initialsInput').value;
-//     getHighScoresScreen();
-// }
-
 function submitScore(userInitials, highScore) {
-    highScoresArr.push({user: {userInitials, highScore}});
-    console.log(highScoresArr);
+    // highScoresArr.push({userInitials, highScore});
+    highScoresArr.push({userInitials, highScore});
+    displayHighScores = highScoresArr;
 }
 
 function handleClick() {
+    //console.log(timerCount);
+    //timerCountDisplay = timerCount + 10;
     initials = document.getElementById('initialsInput').value;
-    submitScore(initials, numCorrect);
+    submitScore(initials, timerCount);
     getHighScoresScreen();
 }
 
-// highScores is an array that will store high scores via the numCorrect and initials var
-var userInitials;
-var highScore;
-var highScoresArr = [];
-
-var gamesPlayed = 0;
-
-
-
 function getHighScoresScreen() {
-    ++gamesPlayed;
+    clearInterval(timer);
+    //questionIndex = 0;
+    timerContainer.innerHTML = ' ';
+    quizContainer.innerHTML = ' ';
+    startContainer.innerHTML = ' ';
     resultsContainer.innerHTML = ' ';
-    gameOverContainer.innerHTML = '<h3>High Scores</h3><p>'+initials+' - '+numCorrect+'</p><input type="button" value="Play Again" id="goBackButton" onclick=""><input type="button" value="Clear High Scores" id="clearHighScoresButton" onclick="">';
+    highScoresDisplay = '';
 
-    // highSchores
-    // highScoresArr.userInitials = initials;
-    // highScoresArr.highScore = numCorrect;
-    // highScoresArr.push({user: {userInitials, highScore}});
-    // //highScoresArr = highScoresArr.concat([initials, numCorrect]);
-    // console.log(highScoresArr);
-
+    // organizes highScoresArr from highest to lowest scores
+    highScoresArr.sort(compare);
+    highScoresArr.reverse();
 
     for (let i = 0; i < highScoresArr.length; i++) {
-        console.log(highScoresArr.userInitials);
-        console.log(highScoresArr.highScore);
+        highScoresDisplay = highScoresDisplay.concat('<li>'+highScoresArr[i].userInitials+' - '+highScoresArr[i].highScore+'</li>');
     }
+
+    updateGameOverContainer(highScoresDisplay);
+
+}
+
+function compare(a, b) {
+    const comparisonA = a.highScore;
+    const comparisonB = b.highScore;
+
+    let comparison = 0;
+    if (comparisonA > comparisonB) {
+        comparison = 1;
+    } else if (comparisonA < comparisonB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function clearHighScores() {
+    highScoresArr = [];
+    highScoresDisplay = '';
+    updateGameOverContainer(highScoresDisplay);
+}
+
+function updateGameOverContainer(a) {
+    gameOverContainer.innerHTML = '<h3>High Scores</h3><br><div class="row"><div class="column"><ul>' + a + '</ul></div></div><br>' + '</p><div style="text-align:center;"><input type="button" value="Play Again" id="goBackButton" onclick="startQuiz()"><br><br><input type="button" value="Clear High Scores" id="clearHighScoresButton" onclick="clearHighScores()"></input></div>';
 }
 
 // Get Answer - gets answer from user input when user selects an answer
 function submitAnswer() {
     
     answerContainers = quizContainer.querySelectorAll('.answers');
-    //answerContainers = quizContainer.getElementById('answers');
-    //console.log(answerContainers,questionIndex);
-    //userAnswer = '';
 
     userAnswer = (answerContainers[0].querySelector('input[name=question'+questionIndex+']:checked')||{}).value;
-    // userAnswer = (answerContainers[questionIndex].querySelector('input[name=question'+questionIndex+']:checked')||{}).value;
         
     // if answer is correct
     if(userAnswer===myQuestions[questionIndex].correctAnswer){
@@ -217,174 +241,29 @@ function submitAnswer() {
         if (timerCount > 0) {
             timerCount = timerCount - 10;
             if (timerCount <= 0) {
+                timerCount = 0;
                 gameOver();
             }
         }
     }
 }
 
-function updateButton() {
-    startButton.disabled = true;
-    if (startButton.value === 'Start Quiz') {
-        startButton.value = 'Stop Quiz';
-    } else {
-        startButton.value = 'Start Quiz';
-    }
-}
-
-/*
-function updateSubmitButton() {
-    if (submitButton === 'Submit Answer') {
-        submitButton = 'Next Question';
-    } else {
-        submitButton = 'Submit Answer';
-    }
-}
-*/
-
 // startQuiz needs to display quesion and answers + update button + start timer
 function startQuiz() {
-    updateButton();
-    timerCount = 30;
+    questionIndex = -1;
+    numCorrect = 0;
+    startContainer.innerHTML = '';
+    gameOverContainer.innerHTML = '';
+    timerCount = 75;
     timerContainer.innerHTML = '<div>Time Remaining: ' + timerCount + '</div>';
     startTimer();
     givePrompts();
 }
 
 // Activates quiz function when startButton is clicked
-startButton.addEventListener("cilck", startQuiz);
+// startButton.addEventListener("click", startQuiz);
 
-
-
-
-
-
-
-
-
-
-
-
-
-// quiz questions function
-function showQuestions() {
-
-    // displays questions and answers
-    for (let i = 0; i < myQuestions.length; i++) {
-        // displays question
-        // [ ] must stop question display so that we don't go to next question without user input/answer
-
-        // resets the list of answers
-        answersList = [];
-        answerButton = [];
-
-        // displays multiple choice answers
-        for(letter in myQuestions[i].answers){
-            // display multiple choice ansswers
-            answersList.push(
-            '<label>'
-                + letter + ': '
-                + '<input type="radio" name="question'+i+'" value="'+myQuestions[i].answers[letter]+'"onclick="submitAnswer()"'+'>'
-                +'<br>'
-            + '</label>'
-            );
-        }
-    
-        // creates submit button
-        //if (questionIndex < (myQuestions.length - 1)) {
-            var nextButton = document.createElement("input");
-            nextButton.type = "button";
-            nextButton.value = "Submit Your Answer";
-            //nextButton.addEventListener('click', nextQuestion);
-            //document.body.appendChild(nextButton);
-        //}
-
-        // add this question and its answers to the output
-        output.push(
-            '<div class="question">' + myQuestions[i].question + '</div>'
-            + '<div class="answers">' + answersList.join('') + '</div>'
-            + '<input type="button" class="submitButton" onclick="submitAnswer()" value="Submit Answer">'
-            //+ '<div>' + nextButton.addEventListener('click', showQuestions)
-            //+ document.body.appendChild(nextButton)
-        );
-
-        quizContainer.innerHTML = output.join('');
-    }
-}
-
-function nextQuestion() {
-    //document.body.innerHTML = '';
-    //quizContainer.innerHTML = output.join('');
-    //contentSectionEl = '';
-    ++questionIndex;
-
-    answersList = [];
-    output = [];
-
-    // writes question
-    quizContainer.innerHTML = myQuestions[questionIndex].question;
-    //document.write(myQuestions[questionIndex].question + "<br>");
-    //contentSectionEl = myQuestions[questionIndex].question + "<br>";
-    //document.innerHTML(myQuestions[questionIndex].question + "<br>");
-
-    // displays multiple choice answers
-    //for (letter in myQuestions[questionIndex].answers) {
-
-        for (let i = 0; i < myQuestions.length; i++) {
-            for(letter in myQuestions[i].answers){
-
-                // fills answers list array with multiple choice ansswers
-                answersList.push(
-                '<label>'
-                    + letter + ': '
-                    + '<input type="radio" name="question'+i+'" value="'+myQuestions[i].answers[letter]+'"onclick="submitAnswer()"'+'>'
-                    +'<br>'
-                + '</label>'
-                );
-            }
-
-            // displays questions, answers, and submit button
-            output.push(
-                '<div class="question">' + myQuestions[i].question + '</div>'
-                + '<div class="answers">' + answersList.join('') + '</div>'
-                + '<input type="button" class="submitButton" onclick="submitAnswer()" value="Submit Answer">'
-            );
-        
-            quizContainer.innerHTML = output.join('');
-
-        }
-
-
-        //document.write("<input type=radio id=myRadio name=userAnswer>" + myQuestions[questionIndex].answers[letter] + '<br>');
-        //contentSectionEl = "<input type=radio id=myRadio name=userAnswer>" + myQuestions[questionIndex].answers[letter] + '<br>';
-        //document.getElementById('quiz').innerHTML = "<input type=radio id=myRadio name=userAnswer>" + myQuestions[questionIndex].answers[letter] + '<br>';
-        //quizContainer.appendChild(document.createTextNode(''));
-    //}
-
-    // creates button and sets up event listener - waiting for user input
-    if (questionIndex < (myQuestions.length - 1)) {
-        var nextButton = document.createElement("input");
-        nextButton.type = "button";
-        nextButton.value = "Next Question [Submit Answer]";
-        nextButton.addEventListener('click', nextQuestion);
-        document.body.appendChild(nextButton);
-    }
-}
-
-//document.querySelector(".start-button").onclick = null;
-
-// if quizOn, we want to run the quiz
-
-
-/*
-if (quizOn) {
-    console.log("quiz is on");
-} else {    // if quiz is off, we want to stop quiz and be able to turn on
-    console.log("quiz is off");
-}
-*/
-
-
+getStartScreen();
 
 /*
 
@@ -393,11 +272,6 @@ GIVEN I am taking a code quiz
 [x] WHEN I answer a question THEN I am presented with another question
 [x] WHEN I answer a question incorrectly THEN time is subtracted from the clock
 [x] WHEN all questions are answered or the timer reaches 0 THEN the game is over
-[ ] WHEN the game is over THEN I can save my initials and my score
-
-PERSONAL:
-[x] how to make it so that when I click "start quiz" button multiple times, it doesn't speed up the count-down? I want it to stop the count-down or not affect it all.
-[ ] get answer to be recorded
-[ ] how to make it so that only one answer can be selected
+[x] WHEN the game is over THEN I can save my initials and my score
 
 */
